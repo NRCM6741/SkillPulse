02216b0f5087d497f24a5574a2636c34caecf194
NRCM6741 <meenapallyarchana@gmail.com>
2025-11-09 12:07:31 +0530

    Added Spring Boot backend project

---
+++ backend/SkillPulseBackend/src/main/java/com/SkillPulse/backend/SkillPulseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.SkillPulse.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillPulseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillPulseBackendApplication.class, args);
+	}
+
+}
---
+++ backend/SkillPulseBackend/src/test/java/com/SkillPulse/backend/SkillPulseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SkillPulse.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillPulseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
